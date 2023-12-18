da8f5d08b34280d196c0c0f25c7d466fee461dde
ginopastran <ginopastran@gmail.com>
2023-12-18 20:11:41 -0300

    Webhook bug fixed

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,51 +1,69 @@
-import { NextResponse } from "next/server";
-import prismadb from "@/lib/prismadb";
-import mercadopago from "mercadopago";
+// import Stripe from "stripe"
+// import { headers } from "next/headers"
+// import { NextResponse } from "next/server"
 
-mercadopago.configure({
-  access_token: process.env.NEXT_ACCESS_TOKEN!,
-});
+// import { stripe } from "@/lib/stripe"
+// import prismadb from "@/lib/prismadb"
 
-export async function POST(req: Request) {
-  const paymentId = await req.text();
+// export async function POST(req: Request) {
+//   const body = await req.text()
+//   const signature = headers().get("Stripe-Signature") as string
 
-  let payment;
-  try {
-    payment = await mercadopago.payment.findById(paymentId);
-  } catch (error: any) {
-    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
-  }
+//   let event: Stripe.Event
 
-  if (payment.status === "approved") {
-    const order = await prismadb.order.update({
-      where: {
-        id: payment.order.id,
-      },
-      data: {
-        isPaid: true,
-        // MercadoPago no proporciona detalles de la dirección del cliente en el objeto de pago
-        // Deberías obtener estos detalles de otra manera
-        address: '',
-        phone: '',
-      },
-      include: {
-        orderItems: true,
-      }
-    });
+//   try {
+//     event = stripe.webhooks.constructEvent(
+//       body,
+//       signature,
+//       process.env.STRIPE_WEBHOOK_SECRET!
+//     )
+//   } catch (error: any) {
+//     return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 })
+//   }
 
-    const productIds = order.orderItems.map((orderItem) => orderItem.productId);
+//   const session = event.data.object as Stripe.Checkout.Session;
+//   const address = session?.customer_details?.address;
 
-    await prismadb.product.updateMany({
-      where: {
-        id: {
-          in: [...productIds],
-        },
-      },
-      data: {
-        isArchived: true
-      }
-    });
-  }
+//   const addressComponents = [
+//     address?.line1,
+//     address?.line2,
+//     address?.city,
+//     address?.state,
+//     address?.postal_code,
+//     address?.country
+//   ];
 
-  return new NextResponse(null, { status: 200 });
-};
+//   const addressString = addressComponents.filter((c) => c !== null).join(', ');
+
+
+//   if (event.type === "checkout.session.completed") {
+//     const order = await prismadb.order.update({
+//       where: {
+//         id: session?.metadata?.orderId,
+//       },
+//       data: {
+//         isPaid: true,
+//         address: addressString,
+//         phone: session?.customer_details?.phone || '',
+//       },
+//       include: {
+//         orderItems: true,
+//       }
+//     });
+
+//     const productIds = order.orderItems.map((orderItem) => orderItem.productId);
+
+//     await prismadb.product.updateMany({
+//       where: {
+//         id: {
+//           in: [...productIds],
+//         },
+//       },
+//       data: {
+//         isArchived: true
+//       }
+//     });
+//   }
+
+//   return new NextResponse(null, { status: 200 });
+// };
