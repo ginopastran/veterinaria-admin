1239aa59dff98dac5bfb20249c5f7967b8b0536f
ginopastran <ginopastran@gmail.com>
2024-01-10 11:03:48 -0300

    Webhook within checkout folder

---
+++ app/api/[storeId]/checkout/webhook/route.ts
@@ -0,0 +1,43 @@
+import prismadb from "@/lib/prismadb";
+import mercadopago from "mercadopago";
+import { NextRequest, NextResponse } from "next/server";
+
+mercadopago.configure({
+    access_token: process.env.NEXT_ACCESS_TOKEN!,
+});
+
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+    return NextResponse.json({}, { headers: corsHeaders });
+}
+
+export async function POST(req: NextRequest) {
+    try {
+        const {
+            type,
+            data: { id },
+        } = await req.json();
+
+        if (type == "payment") {
+            const payment = await mercadopago.payment.findById(Number(id));
+            if (payment.body.status == "approved") {
+                const orderId = payment.body.order.id;
+                await prismadb.order.update({
+                    where: {
+                        id: orderId
+                    },
+                    data: {
+                        isPaid: true
+                    }
+                });
+            }
+        }
+    } catch (error: any) {
+        return NextResponse.json({ message: "An error ocurred", error: error.message }, { status: 500 });
+    }
+}
