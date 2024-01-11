78f5572c69af7f0e1ca52ce3f3bbc84181722188
ginopastran <ginopastran@gmail.com>
2024-01-11 10:39:19 -0300

    localhost cors

--- next.config.js
+++ next.config.js
@@ -9,7 +9,8 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://veterinaria-luffi.vercel.app",            
+            // value: "https://veterinaria-luffi.vercel.app",
+            value: "http://localhost:3000",
           }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
